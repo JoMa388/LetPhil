a9d52efe2171e56d529bf8bef9e31d2a843fcc9a
Jonathan <jonathanma388@gmail.com>
2025-05-14 21:05:03 -0400

    Added Hobby HW

---
+++ 05_homework_hobby_page/script.js
@@ -0,0 +1,7 @@
+let mainText = document.getElementById("mainText");
+let hiddenText = document.getElementById("hiddenText");
+
+function expandText() {
+    mainText.style.display = "none";
+    hiddenText.style.display = "flex";
+}
