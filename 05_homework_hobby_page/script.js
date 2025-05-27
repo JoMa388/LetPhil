7b5e53c7727f65996f70d60512d43a912445e458
Jonathan <jonathanma388@gmail.com>
2025-05-27 17:22:06 -0400

    Cleaned branch

--- 05_homework_hobby_page/script.js
+++
@@ -1,7 +0,0 @@
-let mainText = document.getElementById("mainText");
-let hiddenText = document.getElementById("hiddenText");
-
-function expandText() {
-    mainText.style.display = "none";
-    hiddenText.style.display = "flex";
-}
